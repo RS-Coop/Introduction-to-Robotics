887f39b800a15edbfe75791a5d49ada1359254ef
RS-Coop <simpson.r.cooper@gmail.com>
2019-08-28 16:28:24 -0600

    Should be most updated

--- lab_1_code/lab_1_code.ino
+++ lab_1_code/lab_1_code.ino
@@ -4,9 +4,10 @@
 #define ROTATE 1
 #define DRIVE_OBJ 2
 #define GRAB 3
-#define DRIVE_LINE 4
-#define FOLLOW_LINE 5
-#define STOP 6
+#define TURN_AROUND 4
+#define DRIVE_LINE 5
+#define FOLLOW_LINE 6
+#define STOP 7
 
 // Set up some global variables with default values to be replaced during operation
 int current_state = ROTATE;
@@ -19,6 +20,7 @@ int line_right = 1000;
 void rotate();
 void drive_obj();
 void grab();
+void turn_around();
 void drive_line();
 void follow_line();
 void stop_sparki();
@@ -54,31 +56,34 @@ void loop() {
   sparki.clearLCD();
   sparki.print("STATE: ");
   sparki.println(current_state);
-  
+
   switch (current_state)
   {
     case ROTATE:
       rotate();
       break;
-    
+
     case DRIVE_OBJ:
       drive_obj();
       break;
-    
+
     case GRAB:
       grab();
       break;
-    
+    case TURN_AROUND:
+      // turn 180 degrees
+      turn_around();
+      break;
     case DRIVE_LINE:
       // Drive until you detect a line
       drive_line();
       break;
-    
+
     case FOLLOW_LINE:
       // follow the detected line
       follow_line();
       break;
-    
+
     case STOP:
       // Finished, stop the robot
       stop_sparki();
@@ -90,16 +95,16 @@ void loop() {
   delay(100); // Only run controller at 10Hz
 }
 
-void rotate() 
+void rotate()
 {
   // Test transition "detectObj <= 30cm"
   // if object detected within 30 cm
-  if (sonic_distance != -1 && sonic_distance <= 30) 
+  if (sonic_distance != -1 && sonic_distance <= 30)
   {
     // if true, set state to "object found"
     current_state = DRIVE_OBJ;
   }
-  else 
+  else
   {
     // rotate 5 degrees
     sparki.moveLeft(5);
@@ -112,25 +117,57 @@ void drive_obj()
 {
   // If the object is within 7 cm
     // change state to grab
-    
+
     // else, drive one more cm
   return;
 }
 
-void grab()
+void grab() //Coop
 {
   // Move forward slightly
+  sparki.moveForward(2);
   // Perform grab motion
+  sparki.gripperClose();
+
   return;
 }
 
+// Turn 180 degrees
+void turn_around()
+{
+  sparki.moveLeft(180);
+  current_state = DRIVE_LINE
+}
+
+// Drive until the line is detected
 void drive_line()
 {
+  // Check if the line is detected
+  if (line_left >= threshold || line_right >= threshold || line_center >= threshold) {
+    // Change state to FOLLOW_LINE
+    current_state = FOLLOW_LINE
+  }
+  else
+  {
+    // else, no line detected, drive forward
+    sparki.moveForward(2);
+  }
   return;
 }
 
 void follow_line()
 {
+  // If all line detectos read high line readings, stop (you have reached the finish)
+  if((line_left < threshold) && (line_right < threshold) && (line_center < threshold))
+  {
+    current_state = STOP;
+    return;
+  }
+
+    //else, stear the robot:
+        // If the center detecter is strongest, go strait
+        // else if the left sensor is the strongest, turn left
+        // else if the right sensor is strongest, turn right
   return;
 }
 
