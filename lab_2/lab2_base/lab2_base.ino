c395dce2e2803bb0ea1f8519f8e5d1a45953f1bc
RS-Coop <simpson.r.cooper@gmail.com>
2019-09-17 18:41:12 -0600

    Print in degrees

--- lab_2/lab2_base/lab2_base.ino
+++ lab_2/lab2_base/lab2_base.ino
@@ -89,7 +89,7 @@ void displayOdometry() {
   sparki.print("pose_y");
   sparki.println(pose_y);
   sparki.print("pose_theta");
-  sparki.println(pose_theta);
+  sparki.println(pose_theta*180/PI);
   sparki.updateLCD();
 }
 
@@ -138,39 +138,39 @@ void loop() {
 //  sparki.moveStop();
 
   int threshold = 500;
- 
+
   int lineLeft   = sparki.lineLeft();   // measure the left IR sensor
   int lineCenter = sparki.lineCenter(); // measure the center IR sensor
   int lineRight  = sparki.lineRight();  // measure the right IR sensor
- 
+
   if ( lineLeft < threshold ) // if line is below left line sensor
-  {  
+  {
     sparki.moveLeft(); // turn left
   }
- 
+
   if ( lineRight < threshold ) // if line is below right line sensor
-  {  
+  {
     sparki.moveRight(); // turn right
   }
- 
+
   // if the center line sensor is the only one reading a line
   if ( (lineCenter < threshold) && (lineLeft > threshold) && (lineRight > threshold) )
   {
     sparki.moveForward(); // move forward
-  }  
- 
+  }
+
   sparki.clearLCD(); // wipe the screen
- 
+
   sparki.print("Line Left: "); // show left line sensor on screen
   sparki.println(lineLeft);
- 
+
   sparki.print("Line Center: "); // show center line sensor on screen
   sparki.println(lineCenter);
- 
+
   sparki.print("Line Right: "); // show right line sensor on screen
   sparki.println(lineRight);
- 
+
   sparki.updateLCD(); // display all of the information written to the screen
- 
+
   delay(100); // wait 0.1 seconds
 }
