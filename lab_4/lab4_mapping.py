95c21d98bdc4fd4cb7b0371cd6ffbc2c9c0a3b86
Adam Austin <adam.marcus.austion@gmail.com>
2019-10-22 14:47:53 -0600

    Added admisible heuristic for cost funciton.

--- lab_4/lab4_mapping.py
+++ lab_4/lab4_mapping.py
@@ -205,7 +205,11 @@ def cell_index_to_ij(cell_index):
 
 def cost(cell_index_from, cell_index_to):
     #TODO: Return cost of traversing from one cell to another
-    return 0
+    # This should be an admissible heuristic. Since this doesn't account for obsticles
+    # the manhattan distance should always be optimistic.
+    start_i, start_j = cell_index_to_ij(cell_index_from)
+    dest_i, dest_j = cell_index_to_ij(cell_index_to)
+    return abs(start_i - dest_i) + abs(start_j - dest_j)
 
 if __name__ == "__main__":
     main()
