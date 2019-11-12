459554774185b92594a6c400d20a1d3d32ecc231
RS-Coop <simpson.r.cooper@gmail.com>
2019-11-11 20:57:57 -0700

    Preliminary pub/sub stuff for single dron

--- Multi_Agent_Mapping/Drone_Control/drone_control.py
+++ Multi_Agent_Mapping/Drone_Control/drone_control.py
@@ -1,13 +1,30 @@
-
+import rospy
+from std_msgs.msg import Empty
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import JointState
 #This class deals with interactions between a single drone
 #Currently will only support kinematic related pubs/subs
 class DroneController:
 
     def __init__(self):
+        #Initiate pubs and subs for single drone
+        #NOTE: Need to determine if we need specific
+        #publisher for land and takeoff.
+        self.pilot_pub = rospy.Publisher('', Twist, queue_size=1)
+        self.camera_pub = rospy.Publisher('', JointState, queue_size=1)
+
+        self.odom_sub = rospy.Subscriber('', Odometry, odom_callback)
+        self.camera_sub = rospy.Subscriber('', JointState, camera_callback)
+
+    def odom_callback():
+        pass
+
+    def camera_callback():
         pass
 
 class CentralNode:
-    drones = []
+    drones = [] #List of DroneController objects
 
     def __init__(self):
         pass
