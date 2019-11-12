c097516948b4c4dc7c72187f01f29795aa87787e
RS-Coop <simpson.r.cooper@gmail.com>
2019-11-11 17:44:51 -0700

    Starting to define classes

---
+++ Multi_Agent_Mapping/Drone_Control/drone_control.py
@@ -0,0 +1,13 @@
+
+#This class deals with interactions between a single drone
+#Currently will only support kinematic related pubs/subs
+class DroneController:
+
+    def __init__(self):
+        pass
+
+class CentralNode:
+    drones = []
+
+    def __init__(self):
+        pass
---
+++ Multi_Agent_Mapping/multi_agent.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import rospy
+import Drone_Control as dc
+
+#This is the main script that will execute our project
+def main():
+    print('Right now this is just a test')
+
+#Alternative test function
+def test():
+    CandC = dc.CentralNode()
+
+if __name__=='__main__':
+    test()
+    # main()
